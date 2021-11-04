6eec5c53a297a7a09292f5cea1254b83293aa4dc
Joaquín Colonnello <jcolonnello@itba.edu.ar>
2021-11-04 18:15:01 -0300

    Added some prototypes and consignas

---
+++ src/arguments/configuration.h
@@ -0,0 +1,16 @@
+#pragma once
+
+typedef struct 
+{
+	const char *logFile;
+	const struct sockaddr *listenAddr;
+	const struct sockaddr *mgmtAddr;
+	const char *mgmtPort;
+	const struct sockaddr *listenPort;
+	const char *originPort;
+	const char *filterCmd;
+	const char *originServer;
+} ServerArguments;
+
+extern ServerArguments serverArguments; 
+ServerArguments parseArguments(int argc, char * argv[]);
---
+++ src/net-utils/connect.h
@@ -0,0 +1,15 @@
+#pragma once
+
+enum ConnectionResult
+{
+	CONNECTION_ONGOING,
+	CONNECTION_FAILED,
+	CONNECTION_COMPLETED
+};
+
+//Crea una tarea de conexión, devuelve numero de la tarea
+int startTCPConnection(const char *hostname, const char *port);
+//Le paso el numero de tarea, devuelve el estado de la tarea
+//Cuando falla o completa libera los recursos de la tarea
+//Si completa devuelve fd
+enum ConnectionResult connectToServer(int task, int *fd);
