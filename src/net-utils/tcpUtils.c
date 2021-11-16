25e4e7f277fce55f1b47221d71753cd461606847
Joaquín Colonnello <jcolonnello@itba.edu.ar>
2021-11-16 15:03:14 -0300

    Minimal non-blocking example with epoll

--- src/net-utils/tcpUtils.c
+++ src/net-utils/tcpUtils.c
@@ -1,68 +1,82 @@
 #include "tcpUtils.h"
-#include <errno.h>
-#include <unistd.h>
+#include "logger.h"
 #include <arpa/inet.h>
+#include <asm-generic/errno.h>
+#include <asm-generic/socket.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <netdb.h>
+#include <stddef.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#define MAXPENDING 512 // Maximum outstanding connection requests
+#define MAXPENDING 64 // Maximum outstanding connection requests
 #define MAX_ADDR_BUFFER 128
 
-
-
-int setupTCPServerSocket(const char *service) {
+int setupTCPServerSocket(const char *service)
+{
 
 	// Construct the server address structure
-	struct addrinfo addrCriteria;					// Criteria for address match
+	struct addrinfo addrCriteria;                   // Criteria for address match
 	memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
-	addrCriteria.ai_family = AF_UNSPEC;				// Any address family
-	addrCriteria.ai_flags = AI_PASSIVE;				// Accept on any address/port
-	addrCriteria.ai_socktype = SOCK_STREAM;			// Only stream sockets
-	addrCriteria.ai_protocol = IPPROTO_TCP;			// Only TCP protocol
+	addrCriteria.ai_family = AF_UNSPEC;             // Any address family
+	addrCriteria.ai_flags = AI_PASSIVE;             // Accept on any address/port
+	addrCriteria.ai_socktype = SOCK_STREAM;         // Only stream sockets
+	addrCriteria.ai_protocol = IPPROTO_TCP;         // Only TCP protocol
 
-	struct addrinfo *servAddr;			// List of server addresses
-	int rtnVal = getaddrinfo(NULL, service, &addrCriteria, &servAddr);	//Lista de ips del servidor en servAddr
-	if (rtnVal != 0) {
-		//getaddrinfo failed
+	struct addrinfo *servAddr;                                         // List of server addresses
+	int rtnVal = getaddrinfo(NULL, service, &addrCriteria, &servAddr); // Lista de ips del servidor en servAddr
+	if (rtnVal != 0)
+	{
+		// getaddrinfo failed
 		return -1;
 	}
 
 	int opt = 1;
 	int servSock = -1;
-	// Intentamos ponernos a escuchar en alguno de los puertos asociados al servicio, sin especificar una IP en particular
-	// Iteramos y hacemos el bind por alguna de ellas, la primera que funcione, ya sea la general para IPv4 (0.0.0.0) o IPv6 (::/0) .
-	// Con esta implementación estaremos escuchando o bien en IPv4 o en IPv6, pero no en ambas
-	for (struct addrinfo *addr = servAddr; addr != NULL && servSock == -1; addr = addr->ai_next) {
+	// Intentamos ponernos a escuchar en alguno de los puertos asociados al servicio, sin especificar una IP en
+	// particular Iteramos y hacemos el bind por alguna de ellas, la primera que funcione, ya sea la general para IPv4
+	// (0.0.0.0) o IPv6 (::/0) . Con esta implementación estaremos escuchando o bien en IPv4 o en IPv6, pero no en ambas
+	for (struct addrinfo *addr = servAddr; addr != NULL && servSock == -1; addr = addr->ai_next)
+	{
 		errno = 0;
 		// Create a TCP socket
 		servSock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
-		if (servSock < 0) {
-			//no se pudo crear el socket
-			//Se prueba con el siguiente en la lista
-			continue;		// Socket creation failed; try next address
+		if (servSock < 0)
+		{
+			// no se pudo crear el socket
+			// Se prueba con el siguiente en la lista
+			continue; // Socket creation failed; try next address
 		}
 
-		if( setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
+		if (setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
 		{
 			perror("setsockopt");
 			goto error;
 		}
 
+		// Set nonblocking
+		fcntl(servSock, F_SETFL, O_NONBLOCK);
+
 		// Bind to ALL the address and set socket to listen
-		if ((bind(servSock, addr->ai_addr, addr->ai_addrlen) == 0) && (listen(servSock, MAXPENDING) == 0)) {
-			// Print local address of socket
-			struct sockaddr_storage localAddr;
-			socklen_t addrSize = sizeof(localAddr);
-			if (getsockname(servSock, (struct sockaddr *) &localAddr, &addrSize) >= 0) {
-				//bindeando
-			}
-		} else 
+		if ((bind(servSock, addr->ai_addr, addr->ai_addrlen) == 0) && (listen(servSock, MAXPENDING) == 0))
+		{
+			// // Print local address of socket
+			// struct sockaddr_storage localAddr;
+			// socklen_t addrSize = sizeof(localAddr);
+			// if (getsockname(servSock, (struct sockaddr *)&localAddr, &addrSize) >= 0)
+			// {
+			// 	// bindeando
+			// }
+		}
+		else
 			goto error;
 
 		continue;
-		error:
-		//Manejo de error, fallo en bind
-		close(servSock);  // Close and try with the next one
+	error:
+		// Manejo de error, fallo en bind
+		close(servSock); // Close and try with the next one
 		servSock = -1;
 	}
 
@@ -71,21 +85,31 @@ int setupTCPServerSocket(const char *service) {
 	return servSock;
 }
 
+int acceptTCPConnection(int servSock)
+{
 
-
-
-int acceptTCPConnection(int servSock) {
-	
 	struct sockaddr_storage clntAddr; // Client address
 	// Set length of client address structure (in-out parameter)
 	socklen_t clntAddrLen = sizeof(clntAddr);
 
 	// Wait for a client to connect
-	int clntSock = accept(servSock, (struct sockaddr *) &clntAddr, &clntAddrLen);
-	if (clntSock < 0) {
-		//Manejar error, fallo accept
-		return -1;
+	int clntSock = accept(servSock, (struct sockaddr *)&clntAddr, &clntAddrLen);
+	if (clntSock < 0)
+	{
+		// No pending connections
+		if (errno == EAGAIN || errno == EWOULDBLOCK)
+			return -1;
+		else
+			log(FATAL, "accept failed");
 	}
+
+	/// TODO: Set nonblocking and buffer sizes
+	int buffer = CLIENT_WRITE_BUF * 2;
+	size_t optSize = sizeof(buffer);
+	fcntl(clntSock, F_SETFL, O_NONBLOCK);
+	setsockopt(clntSock, SOL_SOCKET, SO_SNDBUF, &buffer, optSize);
+	buffer = CLIENT_WRITE_BUF;
+	setsockopt(clntSock, SOL_SOCKET, SO_SNDLOWAT, &buffer, optSize);
 
 	// clntSock is connected to a client!
 	return clntSock;
--- src/server.c
+++ src/server.c
@@ -1,136 +1,234 @@
+#define _GNU_SOURCE
+
 #include <errno.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-
-#include <assert.h>
-#include <stddef.h>
-#include <string.h>
-#include <sys/select.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <unistd.h>
 
-#include "net-utils/tcpUtils.h"
+#include "collections/pool.h"
+#include "collections/queue.h"
 #include "logger.h"
-#include "parsers/lexer.h"
+#include "net-utils/tcpUtils.h"
 
-#define MAX_CLIENTS 511
-#define MAX_INPUT 1024
-#define DEFAULT_PORT "9999"
+static int tcpSocket;
+static int epollfd;
+static bool sigioPending = false;
 
-#define MAX(x,y) ((x) > (y)) ? (x) : (y)
+static Queue *eventQueue;
+static Pool *evDataPool;
+static Pool *clients;
 
 typedef struct
 {
-	int socket;
-	Input tcpState;
+	Handle clientEvent;
+	Handle serverEvent;
+	bool active;
 } ClientData;
 
-static ClientData *clients[MAX_CLIENTS];
-static int socketTCP;
+typedef enum
+{
+	ST_PASSIVE,
+	ST_CLIENT,
+	ST_SERVER,
+} SocketType;
+
+typedef struct
+{
+	int fd;
+	Handle clientId;
+	SocketType type;
+	bool readReady, writeReady;
+} EventData;
+
+static Handle eventMod(EventData eventData, uint32_t epollFlags)
+{
+	Handle eventId = Pool_Add(evDataPool, &eventData);
+	if (eventId < 0)
+		log(FATAL, "Out of memory");
+
+	struct epoll_event ev = (struct epoll_event){
+	    .events = epollFlags | EPOLLET,
+	    .data.u32 = eventId,
+	};
+	if (epoll_ctl(epollfd, EPOLL_CTL_ADD, eventData.fd, &ev) == -1)
+		log(FATAL, "epoll_ctl: conn_sock");
+
+	return eventId;
+}
+
+static bool processEvent(struct epoll_event event)
+{
+	EventData *eventData = Pool_GetRef(evDataPool, event.data.u32);
+	if (eventData->fd == tcpSocket && event.events & EPOLLIN)
+	{
+		int clientSocket = acceptTCPConnection(tcpSocket);
+		// No pending connection
+		if (clientSocket < 0)
+		{
+			eventData->readReady = false;
+			return true;
+		}
+
+		ClientData clientData = {
+		    .serverEvent = -1,
+		    .active = true,
+		};
+		Handle clientId = Pool_Add(clients, &clientData);
+		if (clientId < 0)
+			log(FATAL, "Out of memory");
+
+		// Add to epoll
+		Handle clientEvent = eventMod(
+		    (EventData){
+		        .clientId = clientId,
+		        .fd = clientSocket,
+		        .type = ST_CLIENT,
+		    },
+		    EPOLLIN | EPOLLOUT);
+		// Set backward reference
+		ClientData *ref = Pool_GetRef(clients, clientId);
+		ref->clientEvent = clientEvent;
+
+		// Could have more clients waiting, reschedule
+		eventData->readReady = true;
+		return false;
+	}
+	else if (eventData->type)
+	{
+		if (event.events & EPOLLOUT)
+			eventData->writeReady = true;
+		if (event.events & EPOLLIN)
+		{
+			ClientData *client = Pool_GetRef(clients, eventData->clientId);
+			EventData *serverEvent = Pool_GetRef(evDataPool, client->serverEvent);
+			// If server is not ready for write, reschedule
+			if (serverEvent != NULL && !serverEvent->writeReady)
+				return false;
+
+			bool exhausted;
+			/// TODO: Process client stream;
+			// Skip
+			static char buffer[CLIENT_WRITE_BUF];
+			ssize_t skip = read(eventData->fd, buffer, sizeof(buffer));
+			if (skip <= 0)
+			{
+				// Close client
+				close(eventData->fd);
+				Pool_Remove(evDataPool, event.data.u32);
+				client->clientEvent = -1;
+				client->active = false;
+				return true;
+			}
+			exhausted = (skip < CLIENT_WRITE_BUF);
+			eventData->readReady = !exhausted;
+			return exhausted;
+			// perror("OOf");
+			// log(ERROR, "%d", errno);
+		}
+		return true;
+	}
+
+	log(ERROR, "Unhandled event");
+	return true;
+}
+
+static void sigioHandler(int signal)
+{
+	sigioPending = true;
+}
 
 void startServer(const char *port)
 {
-	log(INFO, "Arranco");
+	// Close stdio
+	close(STDIN_FILENO);
+	open("/dev/null", O_WRONLY);
 
-	if(port == NULL)
-		port = DEFAULT_PORT;
+	tcpSocket = setupTCPServerSocket(port);
+	if (tcpSocket < 0)
+		log(FATAL, "Cannot open TCP socket");
 
-	log(DEBUG, "Listen on port: %s", port);
-	socketTCP = setupTCPServerSocket(port);
-	if(socketTCP < 0)
-	{
-		log(ERROR, "Cannot open TCP socket");
-		exit(1);
-	}
+	// Set signal handler for SIGIO
+	struct sigaction handler = (struct sigaction){.sa_handler = sigioHandler};
+	sigfillset(&handler.sa_mask);
+	if (sigaction(SIGIO, &handler, NULL) < 0)
+		log(FATAL, "sigaction() failed for SIGIO");
+	// We must own the socket to receive the SIGIO message
+	if (fcntl(tcpSocket, F_SETOWN, getpid()) < 0)
+		fprintf(stderr, "Unable to set process owner to us");
 
-	memset(clients, 0, sizeof(clients));
+	// Enable SIGIO
+	sigset_t mask, oldmask;
+	sigemptyset(&mask);
+	sigaddset(&mask, SI_SIGIO);
+	sigprocmask(SIG_UNBLOCK, &mask, &oldmask);
+
+	// Create epoll fd
+	epollfd = epoll_create1(0);
+	if (epollfd == -1)
+		log(FATAL, "Failed creating epoll handler");
+
+	// Create collections
+	evDataPool = Pool_Create(sizeof(EventData));
+	eventQueue = Queue_Create(0, sizeof(struct epoll_event));
+	clients = Pool_Create(sizeof(ClientData));
+
+	// Add passive socket to epoll
+	eventMod(
+	    (EventData){
+	        .fd = tcpSocket,
+	        .type = ST_PASSIVE,
+	    },
+	    EPOLLIN);
 }
 
 void processingLoop()
 {
-	fd_set readfds, writefds;
-
-	for(;;)
+	sigset_t sigioMask;
+	sigemptyset(&sigioMask);
+	sigaddset(&sigioMask, SI_SIGIO);
+	for (;;)
 	{
-		FD_ZERO(&readfds); 
-		FD_ZERO(&writefds); 
-		FD_SET(socketTCP, &readfds);
-		int max_sd = socketTCP;			 //Necesito el maximo para el select
+		const int max_events = 10;
+		struct epoll_event events[max_events];
 
-		//Add clients
-		for(int i = 0; i < MAX_CLIENTS; i++) { 
-			if(clients[i] == NULL)
-				continue;
-
-			int sd = clients[i]->socket;
-			FD_SET(sd, clients[i]->tcpState.writeCount > 0 ? &writefds : &readfds);
-			max_sd = MAX(max_sd, sd);
+		// Block and check for SIGIO event
+		sigset_t oldMask;
+		sigprocmask(SIG_BLOCK, &sigioMask, &oldMask);
+		if (sigioPending)
+		{
+			/// TODO: Process signal
+			sigioPending = false;
 		}
 
-		//Espero la actividad de algun sockdet
-		int ready = select(max_sd + 1, &readfds, &writefds, NULL, NULL);   
-		if(ready < 0 && (errno!=EINTR)) {
-			log(ERROR, "select() failed: %s", strerror(errno));
+		// Wait for events
+		int nfds = epoll_pwait(epollfd, events, max_events, -1, &oldMask);
+		if (nfds == -1)
+		{
+			if (errno == EINTR)
+				nfds = 0;
+			else
+				log(FATAL, "epoll_pwait");
 		}
 
-		//New TCP connections
-		if(FD_ISSET(socketTCP, &readfds)) {
-			log(INFO, "Entro en FD_ISSET");
+		// Re-enable SIGIO
+		sigprocmask(SIG_UNBLOCK, &sigioMask, NULL);
 
-			for(int i = 0; i < MAX_CLIENTS; i++){
-				if(clients[i] == NULL) {
-					int newSocket = acceptTCPConnection(socketTCP);
-					if(newSocket < 0)
-						break;
+		// Add events to queue
+		for (int i = 0; i < nfds; i++)
+			Queue_Enqueue(eventQueue, &events[i]);
 
-					clients[i] = malloc(sizeof(ClientData));
-					if(clients[i]) {} //TODO: manejar error
-
-					clients[i]->socket = newSocket;
-					initState(&clients[i]->tcpState);
-					break;
-				}
-			}
-		}
-
-		//TCP Clients
-		for(int i = 0; i < MAX_CLIENTS; i++){
-			ClientData *client = clients[i];
-			bool closeClient = false;
-
-			if(client == NULL)
-				continue;
-
-			int sd = client->socket;
-			if(FD_ISSET(sd, &readfds)){
-				size_t valread = processTcpInput(&client->tcpState, client->socket, MAX_INPUT);
-				if(valread == 0)
-					closeClient = true;
-			}
-			else if(FD_ISSET(sd, &writefds))
-			{
-				size_t count = client->tcpState.writeCount;
-				char *buffer = client->tcpState.writeBuf;
-				size_t sent = write(client->socket, buffer, count);
-				if(sent < 0)
-					closeClient = true;
-				else if(sent < count)
-					memmove(buffer, &buffer[sent], count - sent);
-				client->tcpState.writeCount -= sent;
-			}
-
-			if(closeClient)
-			{
-				close(sd);
-				free(client);
-				clients[i] = NULL;
-				log(INFO, "Closed client: %d", i);
-			}
+		// Process events
+		for (int i = Queue_Count(eventQueue); i > 0; i--)
+		{
+			struct epoll_event event;
+			Queue_Dequeue(eventQueue, &event);
+			bool handled = processEvent(event);
+			if (!handled)
+				Queue_Enqueue(eventQueue, &event);
 		}
 	}
-	return;
-
 }
